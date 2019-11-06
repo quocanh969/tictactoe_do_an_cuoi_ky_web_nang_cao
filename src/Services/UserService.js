0b61d41b3abf81a3c9f7cfb110ad54684fec8542
quocanh969 <32615718+quocanh969@users.noreply.github.com>
2019-11-06 11:20:03 +0700

    Update UserService.js

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,5 +1,5 @@
-const ApiUrl = "http://localhost:8080";
-//const ApiUrl = "https://server-midtern-project.herokuapp.com";
+//const ApiUrl = "http://localhost:8080";
+const ApiUrl = "https://server-midtern-project.herokuapp.com";
 
 export const us = {
     login,
